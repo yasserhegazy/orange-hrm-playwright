96e9579d168505709c29e2b991038c8ab3046fde
yasserhegazy <yasseranahegazy@gmail.com>
2026-03-07 21:13:16 +0200

    feat: add constants for application configuration

---
+++ data/constants.py
@@ -0,0 +1,10 @@
+BASE_URL = "https://opensource-demo.orangehrmlive.com/"
+
+VALID_USERNAME = "Admin"
+VALID_PASSWORD = "admin123"
+
+EMPLOYEE_FIRST_NAME = "Yasser"
+EMPLOYEE_MIDDLE_NAME = "Osama"
+EMPLOYEE_LAST_NAME = "Hegazy"
+
+LOGIN_PASSWORD = "Test12345@"

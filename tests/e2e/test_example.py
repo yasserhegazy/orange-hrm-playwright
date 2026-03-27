ac906289835bbb54a777ff94724530e0b0f729cf
yasserhegazy <yasseranahegazy@gmail.com>
2026-03-27 22:24:39 +0200

    refactor: simplify login test by utilizing LoginPage class for authentication

--- tests/e2e/test_example.py
+++ tests/e2e/test_example.py
@@ -1,10 +1,11 @@
-import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+
+from data.constants import VALID_PASSWORD, VALID_USERNAME
+from pages.auth.login_page import LoginPage
+from pages.navigation.dashboard_page import DashboardPage
+
 
 def test_login_to_orangehrm(page: Page):
-
-    page.get_by_placeholder("Username").fill("Admin")
-    page.get_by_placeholder("Password").fill("admin123")
-
-    page.get_by_role("button", name="Login").click()
-    expect(page).to_have_title(re.compile("OrangeHRM"))
+    login_page = LoginPage(page)
+    login_page.login(VALID_USERNAME, VALID_PASSWORD)
+    DashboardPage(page)

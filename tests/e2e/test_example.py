c743671e8c09d0ceac05f2a3bc995a72fc0d130c
yasserhegazy <yasseranahegazy@gmail.com>
2026-03-29 00:27:06 +0300

    fix: update login test to assert dashboard page presence after login

--- tests/e2e/test_example.py
+++ tests/e2e/test_example.py
@@ -2,10 +2,9 @@ from playwright.sync_api import Page
 
 from data.constants import VALID_PASSWORD, VALID_USERNAME
 from pages.auth.login_page import LoginPage
-from pages.navigation.dashboard_page import DashboardPage
 
 
 def test_login_to_orangehrm(page: Page):
     login_page = LoginPage(page)
-    login_page.login(VALID_USERNAME, VALID_PASSWORD)
-    DashboardPage(page)
+    dashboard_page = login_page.login(VALID_USERNAME, VALID_PASSWORD)
+    assert dashboard_page is not None

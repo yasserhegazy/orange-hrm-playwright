7030429b1248a06f4b4a35d639a2fc0a79e060b4
yasserhegazy <yasseranahegazy@gmail.com>
2026-04-04 00:03:18 +0300

    feat: add test for vacancy creation with parameterized job titles and statuses

---
+++ tests/e2e/test_vacancy_creation.py
@@ -0,0 +1,38 @@
+import pytest
+
+from data.models import VacancyStatus
+from pages.navigation.side_menu_page import SideMenuPage
+from pages.recruitment.vacancy_list_page import VacancyListPage
+from tests.plugins.vacancy_cleanup import VacancyCleanupTracker
+from tests.utils.generate_vacancy_data import generate_vacancy_data
+
+
+class TestVacancyCreation:
+    @pytest.mark.parametrize(
+        ("job_title", "status"),
+        [
+            ("QA Lead", VacancyStatus.ACTIVE),
+            ("Account Assistant", VacancyStatus.INACTIVE),
+            ("Chief Executive Officer", VacancyStatus.ACTIVE),
+        ],
+        ids=[
+            "active_qa_lead",
+            "inactive_account_assistant",
+            "active_ceo",
+        ],
+    )
+    def test_create_vacancy(
+        self,
+        vacancy_list_page: VacancyListPage,
+        vacancy_cleanup: VacancyCleanupTracker,
+        job_title: str,
+        status: VacancyStatus,
+    ):
+        vacancy_data = generate_vacancy_data(job_title=job_title, status=status)
+        vacancy_cleanup.register(vacancy_data.vacancy_name)
+
+        add_vacancy_page = vacancy_list_page.click_add_vacancy()
+        add_vacancy_page.create_vacancy(vacancy_data)
+
+        vacancy_list = SideMenuPage(vacancy_list_page.page).navigate_to_recruitment().navigate_to_vacancies()
+        assert vacancy_list.has_vacancy(vacancy_data.vacancy_name)
